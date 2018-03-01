15b288df5bcbf041a60123a0426d59577b1e1d5e
gdal@thinkpad <gdal@cs.ru.nl>
2018-03-01 21:00:05 +0100

    removed \0 from c_str() output

--- include/stringf.h
+++ include/stringf.h
@@ -5,11 +5,11 @@
 
 template< typename... argv >
 std::string stringf( const char* format, argv... args ) {
-    const size_t SIZE = std::snprintf( NULL, 0, format, args... );
+    const size_t kSize = std::snprintf( NULL, 0, format, args... );
 
     std::string output;
-    output.resize(SIZE+1);
-    std::snprintf( &(output[0]), SIZE+1, format, args... );
+    output.resize(kSize);
+    std::snprintf( &(output[0]), kSize+1, format, args... );
     return std::move(output);
 }
 
